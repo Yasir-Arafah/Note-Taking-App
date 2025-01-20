2c6fa70db191318c771db853613b689f4e5b482d
Prince <73953652+Yasir-Arafah@users.noreply.github.com>
2025-01-20 17:57:29 +0600

    add files

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Note_Taking_App
+{
+    partial class NoteTaker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NoteTaker));
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.noteBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PreviousNotes = new System.Windows.Forms.DataGridView();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.NewNoteButton = new System.Windows.Forms.Button();
+            this.ReactionImage = new System.Windows.Forms.Panel();
+            this.statusimages = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PreviousNotes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleBox
+            // 
+            this.titleBox.Location = new System.Drawing.Point(293, 173);
+            this.titleBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(372, 20);
+            this.titleBox.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // noteBox
+            // 
+            this.noteBox.Location = new System.Drawing.Point(293, 242);
+            this.noteBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.noteBox.Multiline = true;
+            this.noteBox.Name = "noteBox";
+            this.noteBox.Size = new System.Drawing.Size(371, 350);
+            this.noteBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(292, 135);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Title:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Location = new System.Drawing.Point(292, 205);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(373, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Note:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // PreviousNotes
+            // 
+            this.PreviousNotes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.PreviousNotes.BackgroundColor = System.Drawing.SystemColors.Menu;
+            this.PreviousNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.PreviousNotes.Location = new System.Drawing.Point(9, 10);
+            this.PreviousNotes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.PreviousNotes.Name = "PreviousNotes";
+            this.PreviousNotes.RowHeadersWidth = 51;
+            this.PreviousNotes.RowTemplate.Height = 24;
+            this.PreviousNotes.Size = new System.Drawing.Size(277, 486);
+            this.PreviousNotes.TabIndex = 5;
+            this.PreviousNotes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.PreviousNotes_CellContentClick);
+            this.PreviousNotes.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.PreviousNotes_CellDoubleClick);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.LoadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadButton.Location = new System.Drawing.Point(9, 500);
+            this.LoadButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(128, 46);
+            this.LoadButton.TabIndex = 8;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = false;
+            this.LoadButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.DeleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteButton.Location = new System.Drawing.Point(152, 500);
+            this.DeleteButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(134, 46);
+            this.DeleteButton.TabIndex = 9;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = false;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveButton.Location = new System.Drawing.Point(9, 551);
+            this.SaveButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(128, 46);
+            this.SaveButton.TabIndex = 11;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = false;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // NewNoteButton
+            // 
+            this.NewNoteButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.NewNoteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewNoteButton.Location = new System.Drawing.Point(152, 551);
+            this.NewNoteButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.NewNoteButton.Name = "NewNoteButton";
+            this.NewNoteButton.Size = new System.Drawing.Size(134, 46);
+            this.NewNoteButton.TabIndex = 12;
+            this.NewNoteButton.Text = "New Note";
+            this.NewNoteButton.UseVisualStyleBackColor = false;
+            this.NewNoteButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ReactionImage
+            // 
+            this.ReactionImage.Location = new System.Drawing.Point(354, 21);
+            this.ReactionImage.Name = "ReactionImage";
+            this.ReactionImage.Size = new System.Drawing.Size(264, 100);
+            this.ReactionImage.TabIndex = 13;
+            this.ReactionImage.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // statusimages
+            // 
+            this.statusimages.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("statusimages.ImageStream")));
+            this.statusimages.TransparentColor = System.Drawing.Color.Transparent;
+            this.statusimages.Images.SetKeyName(0, "load.jpg");
+            this.statusimages.Images.SetKeyName(1, "save.jpg");
+            this.statusimages.Images.SetKeyName(2, "delete.jpg");
+            // 
+            // NoteTaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(672, 601);
+            this.Controls.Add(this.ReactionImage);
+            this.Controls.Add(this.NewNoteButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.PreviousNotes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.noteBox);
+            this.Controls.Add(this.titleBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "NoteTaker";
+            this.Text = "Your Personal Note Taker";
+            this.Load += new System.EventHandler(this.NoteTaker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PreviousNotes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox noteBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView PreviousNotes;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button NewNoteButton;
+        private System.Windows.Forms.Panel ReactionImage;
+        private System.Windows.Forms.ImageList statusimages;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Note_Taking_App
+{
+    public partial class NoteTaker : Form
+    {
+        DataTable notes = new DataTable();
+        bool editing = false;
+
+        public NoteTaker()
+        {
+            InitializeComponent();
+        }
+
+        private void NoteTaker_Load(object sender, EventArgs e)
+        {
+            notes.Columns.Add("Title");
+            notes.Columns.Add("Notes");
+
+            PreviousNotes.DataSource = notes;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            titleBox.Text = "";
+            noteBox.Text = "";
+            ReactionImage.BackgroundImage = null;
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                notes.Rows[PreviousNotes.CurrentCell.RowIndex].Delete();
+            }
+            catch(Exception ex) { Console.WriteLine("Not a valid note!!"); }
+
+            ReactionImage.BackgroundImage = statusimages.Images[2];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            titleBox.Text = notes.Rows[PreviousNotes.CurrentCell.RowIndex].ItemArray[0].ToString();
+            noteBox.Text = notes.Rows[PreviousNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
+            editing = true;
+            ReactionImage.BackgroundImage = statusimages.Images[0];
+        }
+
+        private void PreviousNotes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if(editing)
+            {
+                notes.Rows[PreviousNotes.CurrentCell.RowIndex]["Title"] = titleBox.Text;
+                notes.Rows[PreviousNotes.CurrentCell.RowIndex]["Notes"] = noteBox.Text;
+            }
+            else
+            {
+                notes.Rows.Add(titleBox.Text, noteBox.Text);
+            }
+
+            titleBox.Text = "";
+            noteBox.Text = "";
+            editing = false;
+
+            ReactionImage.BackgroundImage = statusimages.Images[1];
+
+        }
+
+        private void PreviousNotes_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            titleBox.Text = notes.Rows[PreviousNotes.CurrentCell.RowIndex].ItemArray[0].ToString();
+            noteBox.Text = notes.Rows[PreviousNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
+            editing = true;
+            ReactionImage.BackgroundImage = statusimages.Images[0];
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
